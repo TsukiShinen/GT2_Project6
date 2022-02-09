5e9e9172b7a8303b05ca272ba0ed6eb17cacf839
OV3RClock <40008329+OV3RClock@users.noreply.github.com>
2022-02-09 12:03:34 +0100

    Text fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void StartGame()
     {
         // Load the grid with the chosen parameters
     }
