84aadc74a788ad0e4adc980ff4c5aff81136b582
OV3RClock <40008329+OV3RClock@users.noreply.github.com>
2022-02-09 11:57:20 +0100

    Menus almost fnished

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // Load the grid with the chosen parameters
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
